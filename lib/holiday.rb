33b943428e08349621063779868090e7edb841b5
Marcus Moreno <legendary.1198@gmail.com>
2020-07-15 21:55:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,13 +2,16 @@ def second_supply_for_fourth_of_july(holiday_hash)
   
   holiday_hash[:summer][:fourth_of_july][1]
   
+  
 end
+
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   
   holiday_hash[:winter].each do |holiday, supplies|
     
     supplies << supply
+    
     
   end
 end
@@ -18,7 +21,9 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   
   holiday_hash[:spring][:memorial_day] << supply
   
+  
 end
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   
@@ -26,13 +31,16 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   
   holiday_hash
 
+
 end
+
 
 def all_winter_holiday_supplies(holiday_hash)
   
   holiday_hash[:winter].map do |holiday, supplies|
     
     supplies
+    
     
   end.flatten
   
